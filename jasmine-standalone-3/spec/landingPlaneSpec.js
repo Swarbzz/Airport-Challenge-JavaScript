3866602dd079a50ec19778761e69faa8091bc07b
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-05-11 11:33:14 +0100

    Added a take off spec

--- jasmine-standalone-3/spec/landingPlaneSpec.js
+++ jasmine-standalone-3/spec/landingPlaneSpec.js
@@ -16,6 +16,12 @@ describe("Airport", function() {
     expect(plane.isLanded()).toEqual(true);
   });
 
+  it("changes the status of a plane to in the air", function() {
+    airport.land(plane);
+    airport.takeoff(plane);
+    expect(plane.isLanded()).toEqual(false);
+  });
+
   it("a plane can take off", function() {
     airport.land(plane);
     airport.takeoff(plane);
